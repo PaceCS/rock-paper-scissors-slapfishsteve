4f2b30fee2859f400f9ed8d63f2369cdcd951be8
gwpjp <john.pearson.02@gmail.com>
2017-11-02 11:48:59 -0400

    Updated skeleton.

--- logic.js
+++ logic.js
@@ -1,9 +1,31 @@
 
 // This array will hold the options for the game
-const options = [];
+// Fill in your options below
+const options = [/*option 1*/, /*option 2*/, /*option 3*/];
 
 // This function takes in the userChoice and then prints the results to the console.
 function playGame(userChoice) {
+
+    // First remind the user of his/her choice
+    console.log(/*Put text here*/);
+
+    // Now choose a random number between 0 and 2
+    const rand = /*Fill in the code for a random value between 0 and 2*/;
+
+    // Then use this random number to give the computer a choice
+    const compChoice = options[rand];
+
+    // Now log the computer's choice to the console.
+    console.log(/*Put text here*/);
+
+    // Now determine the winner using if/else statements
+    /*
+    |
+    |
+    This will take several if/else statements, so it will take multiple lines.
+    |
+    |
+    */
 
 }
 
