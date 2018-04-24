721494a43c451e564d0d89af55768da2b5f7140e
cperchik <32876209+cperchik@users.noreply.github.com>
2018-04-24 08:56:18 -0400

    working on rps

--- logic.js
+++ logic.js
@@ -1,58 +1,41 @@
-
 // This array will hold the options for the game
 // Fill in your options below
-const options = [/*option 1*/, /*option 2*/, /*option 3*/];
+const options = ['Rock', 'Paper', 'Scissors'];
 
 // This function takes in the userChoice and then prints the results to the console.
 function playGame(userChoice) {
-
     // First remind the user of his/her choice
-    console.log(/*Put text here*/);
+    console.log('You chose ' + userChoice);
 
     // Now choose a random number between 0 and 2
-    const rand = /*Fill in the code for a random value between 0 and 2*/;
+    const rand = Math.floor(Math.random() * 3);
 
     // Then use this random number to give the computer a choice
     const compChoice = options[rand];
 
     // Now log the computer's choice to the console.
-    console.log(/*Put text here*/);
+    console.log('The computer chose ' + compChoice);
 
-    // Now determine the winner using if/else statements
-    /*
-    |
-    |
-    This will take several if/else statements, so it will take multiple lines.
-    |
-    |
-    */
-
+    if (userChoice == compChoice) {
+        console.log('You tied');
+    } else if (userChoice == 'Rock') {
+        if (compChoice == 'Paper') {
+            console.log('You lose');
+        } else {
+            console.log('You win!');
+        }
+    } else if (userChoice == 'Paper') {
+        if (compChoice == 'Rock') {
+            console.log('You win!');
+        } else {
+            console.log('You lose');
+        }
+    } else if (compChoice == 'Paper') {
+        console.log('You win!');
+    } else {
+        console.log('You lose');
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports.playGame = playGame;
 module.exports.options = options;
